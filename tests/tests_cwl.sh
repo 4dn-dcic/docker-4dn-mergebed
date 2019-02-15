ec68591035582bf909684713cd4540a53804af89
SooLee <duplexa@gmail.com>
2019-02-15 10:44:40 -0500

    try travis test without .

--- tests/tests_cwl.sh
+++ tests/tests_cwl.sh
@@ -1,4 +1,4 @@
-set -e
+set -eE
 CWL_NAME=$1
 CWL_FILENAME=$1.cwl
 INPUT_JSON_NAME=$1.input.json
@@ -6,7 +6,7 @@ CWD=$(pwd)
 OUTDIR=$CWD/tests/test_outdir
 LOCAL_CWL_TMPDIR=$CWD/tests/test_tmp
 CWL_RUNNER=$(which cwltool)
-RUNNER_FLAGS="--disable-pul --copy-outputs --no-read-only --no-match-user --outdir $OUTDIR --tmp-outdir-prefix $LOCAL_CWL_TMPDIR --tmpdir-prefix $LOCAL_CWL_TMPDIR --non-strict --debug"
+RUNNER_FLAGS="--copy-outputs --no-read-only --no-match-user --outdir $OUTDIR --tmp-outdir-prefix $LOCAL_CWL_TMPDIR --tmpdir-prefix $LOCAL_CWL_TMPDIR --non-strict --debug"
 cd cwl
-$CWL_RUNNER $RUNNER_FLAGS $CWL_FILENAME ../tests/test_input_json/$INPUT_JSON_NAME | python $CWD/tests/json_null_test.py
+$CWL_RUNNER $RUNNER_FLAGS $CWL_FILENAME ../tests/test_input_json/$INPUT_JSON_NAME
 cd ..
