b20259847b08ef58e172f2092e0f78f0d629213b
Soo Lee <duplexa@gmail.com>
2019-02-24 17:18:39 -0500

    Update run-mergebed.sh

--- scripts/run-mergebed.sh
+++ scripts/run-mergebed.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -o pipefail
 outprefix=$1
 shift
 sortv=$1  # if 1: sort chr1, chr2, chr3, if 0: sort chr1, chr10, chr11
@@ -23,7 +22,7 @@ fi
 if [ $NFILES -eq 1 ]
 then
 
-    gunzip -c $INFILESTR | sort $SORT_OPTION | gzip -fc > $outprefix.bed.gz
+    gunzip -c $INFILESTR | sort $SORT_OPTION | gzip -fc > $outprefix.bed.gz || exit
 
 else
 
@@ -34,15 +33,12 @@ else
     do
     mkfifo pp.$k
     arg="$arg pp.$k"
-    gunzip -c $f | sort $SORT_OPTION > pp.$k &
+    gunzip -c $f | sort $SORT_OPTION > pp.$k || exit &
     let "k++"
     done
     
-    # merging 
-    sort -m $SORT_OPTION $arg >> $outprefix.bed
-    
-    # compressing
-    gzip -f $outprefix.bed
+    # merging & compressing
+    sort -m $SORT_OPTION $arg | gzip -fc -> $outprefix.bed || exit
     
     # clean up
     k=1
